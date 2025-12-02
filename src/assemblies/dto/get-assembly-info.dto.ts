d4b3256e70e1a5e448adec828835e26d247c5501
f.burov <f.burov@solber.ru>
2025-12-02 09:26:28 +0300

    Сделал запросы на получение списка пользователей и статистики по пользователям

--- src/assemblies/dto/get-assembly-info.dto.ts
+++ src/assemblies/dto/get-assembly-info.dto.ts
@@ -4,13 +4,14 @@ import { ProductsAssemblies } from '../entities/products-assemblies.entity';
 
 export class GetAssemblyInfoDto {
   static mapModel(model: Assemblies, relationships: ProductsAssemblies[]): GetAssemblyInfo {
-    console.log('model', model);
     return {
       id: model.id,
       article: model.article,
       title: model.title,
       description: model.description,
       products: model.products.map(product => {
+        console.log('productId', product.id)
+        console.log(relationships.find(el => el.productId === product.id))
         return {
           id: product.id,
           article: product.article,
