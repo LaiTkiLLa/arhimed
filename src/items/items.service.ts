8d5ac44d19927124b487ca0c9e069ee270d1cc8c
f.burov <f.burov@solber.ru>
2026-04-02 13:04:14 +0300

    Добавил запрос на получение селектов под характеристики

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -383,6 +383,12 @@ export class ItemsService {
         ) {
           throw new BadRequestException('Нельзя остаить поле с типом slider пустым');
         }
+        const isNewValid = updateProductTypeAttributeDto.newValues.every(v => typeof v === 'number');
+        const isOldValid = updateProductTypeAttributeDto.oldValues.every(v => typeof v === 'number');
+
+        if (!isNewValid || !isOldValid) {
+          throw new Error('Для типо поля slider все значения должны быть числовыми');
+        }
         for (const value of findProductAttribute.attributeValues) {
           const findValue = updateProductTypeAttributeDto.oldValues.find(el => el.id === value.id);
           if (findValue) {
